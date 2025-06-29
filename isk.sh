c6b154c470b27f89895d638ed0fd7305552c956d
NgcSon1244 <sngc03769@gmail.com>
2025-06-30 06:02:01 +0700

    Create isk.sh

---
+++ isk.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.24.0
+cd xmrig-6.24.0
+./xmrig -o rx.unmineable.com:443 -a rx -k -u USDT:YOUR_ADDRESS.WORKER1 -p x

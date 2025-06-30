e4247eda448154477f6c316148bae052f7401beb
NgcSon1244 <sngc03769@gmail.com>
2025-06-30 11:03:26 +0700

    Update isk.sh

--- isk.sh
+++ isk.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
 tar -xvf xmrig-6.24.0-linux-static-x64.tar.gz
 cd xmrig-6.24.0
-./xmrig -o rx.unmineable.com:443 -a rx -k -u USDT:0xcBCfBaE7ED1E38e3B47Ec000B9786a01fEc844F8.WORKER1 -p x
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:0xcBCfBaE7ED1E38e3B47Ec000B9786a01fEc844F8.WORKER1 -p x

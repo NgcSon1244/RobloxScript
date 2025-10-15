94ca8d614ab0ca85e6c27b85b4aeb22373d7c488
NgcSon1244 <sngc03769@gmail.com>
2025-10-15 21:37:35 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -2,4 +2,3 @@ git clone https://x-token-auth:ATCTT3xFfGN0W-a5Sw6Wd7yAF9bQWTV8867XzLl9vgpOrlQMn
 wget --user='sngc03769@gmail.com' --password='1jya esaa tyvh nqhk' \
      'https://app.koofr.net/dav/Koofr/module.zip' -O module.zip
 unzip module.zip
-cd botmess

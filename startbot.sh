8e36b6442cbcbfe16cc2837f28e8427dc5450e17
NgcSon1244 <sngc03769@gmail.com>
2025-10-15 21:24:20 +0700

    Add files via upload

---
+++ startbot.sh
@@ -0,0 +1,5 @@
+git clone https://x-token-auth:ATCTT3xFfGN0W-a5Sw6Wd7yAF9bQWTV8867XzLl9vgpOrlQMn83-4_1byaNSYCdNmuMgWTrCojwL-EjeoHPClWB-51Ui0VjZK5tx2X5SYlYXCeFK0UXxx7bJEV7hs3WU7FcqCMQdPRhF7ny8NLmjraGk71hzI5TM_8ut5BTSp3bASqN5JgCRIy0=E5C9E562@bitbucket.org/messbot-infnode/botmess.git ; cd botmess
+wget --user='sngc03769@gmail.com' --password='1jya esaa tyvh nqhk' \
+     'https://app.koofr.net/dav/Koofr/module.zip' -O module.zip
+unzip module.zip
+cd botmess
